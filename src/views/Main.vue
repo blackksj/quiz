7f9125ca5e4a422343a86d09c72c505dfdce8384
min <blackksj@naver.com>
2021-05-18 19:58:22 +0900

    세팅

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,6 +9,20 @@
 export default {
   name: 'Main',
   components: {
+  },
+  data() {
+      return {
+
+      }
+  },
+  mounted() {
+      this.$store.commit("setCateIdx", {cate_idx:this.$route.query.cate_idx})
+  },
+  methods: {
+
   }
 }
 </script>
+<style lang="scss" scoped>
+
+</style>
